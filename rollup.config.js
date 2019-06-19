7bc05c922f20960057dceef2a68431024a4a371a
DeAndre Johnson <dre@phreshr.com>
2019-06-19 11:40:28 +0400

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,91 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import pkg from './package.json'
 
-const input = './src/index.js'
-const globals = { graphql: 'GraphQL' }
+const external = [
+  'graphql',
+  'svelte',
+  'svelte/store',
+  'gql-cache',
+  'callbag-pipe',
+  'callbag-observe',
+  'tallbag-map',
+  'tallbag-from-fetch',
+  'shadow-callbag',
+  '@sindresorhus/fnv1a'
+]
+
 const babelOpts = {
   exclude: '**/node_modules/**',
   runtimeHelpers: true
 }
 
-module.exports = {
-  input,
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    globals
-  },
-  treeshake: true,
-  external: Object.keys(globals),
-  plugins: [
-    babel(babelOpts),
-    terser({
-      module: true
-    }),
-    sizeSnapshot()
-  ]
-}
+export default [
+  {
+    input: 'src/index.js',
+    external,
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      babel(babelOpts),
+      terser({
+        module: true
+      }),
+      sizeSnapshot()
+    ]
+  }
+]
+
+// const input = './src/index.js'
+// const babelOpts = {
+//   exclude: '**/node_modules/**',
+//   runtimeHelpers: true
+// }
+
+// module.exports = {
+//   input,
+//   output: {
+//     file: pkg.main,
+//     format: 'cjs'
+//   },
+//   treeshake: true,
+//   external: [...Object.keys(pkg.peerDependencies || {})],
+//   plugins: [
+//     resolve({
+//       jsnext: true
+//     }),
+//     commonjs(),
+//     replace({
+//       'process.env.NODE_ENV': JSON.stringify('production')
+//     }),
+//     babel(babelOpts),
+//     terser({
+//       compress: {
+//         pure_getters: true,
+//         unsafe: true,
+//         unsafe_comps: true,
+//         warnings: false
+//       }
+//     }),
+//     sizeSnapshot()
+//   ]
+// }
